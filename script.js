f69e0ff71cd37e3cac7c77df1d04c708ade0ebd7
Ana Monteiro <anamdias@yahoo.com.br>
2022-01-13 08:29:31 -0300

    Requisto 18

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-<<<<<<< HEAD
-=======
 /* eslint-disable no-use-before-define */
 /* eslint-disable no-unused-vars */
 /* eslint-disable prefer-const */
->>>>>>> 9e7b48bf650c929bf29b0a7a0c19069d9415a445
 const btnEntrar = document.querySelector('#btn-entrar');
 const inputEmail = document.querySelector('#email');
 const inputSenha = document.querySelector('#senha');
