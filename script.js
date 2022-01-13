034c9797d427027b752988a0178bcbaa5352708e
EDUARDO FERREIRA <dupearljam@gmail.com>
2022-01-12 23:25:25 -0300

    remove navbar, ajusta estilo do header e funçao do login

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-=======
-/* eslint-disable no-use-before-define */
-/* eslint-disable no-unused-vars */
-/* eslint-disable prefer-const */
->>>>>>> 9e7b48bf650c929bf29b0a7a0c19069d9415a445
 const btnEntrar = document.querySelector('#btn-entrar');
 const inputEmail = document.querySelector('#email');
 const inputSenha = document.querySelector('#senha');
@@ -12,7 +6,7 @@ function logar() {
   if (inputEmail.value === 'tryber@teste.com' && inputSenha.value === '123456') {
     alert('Olá, Tryber!');
   } else {
-    alert('Email ou senha inválidos');
+    alert('Email ou senha inválidos.');
   }
 }
 btnEntrar.addEventListener('click', logar);
